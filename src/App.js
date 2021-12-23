3ef3fb606146bbbbe4585388ee02b49b1b9d0e3e
MatthewClarkOSU <matthewclarkosu@gmail.com>
2021-12-23 15:14:23 -0500

    header spacing set

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import './App.css';
-// import 'font-awesome/css/font-awesome.min.css';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import loupe from "./images/loupe.png"
 import cat_image1_mobile from "./images/cat_image1_mobile.png"
-// import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <button class="burger" onclick="this.classList.toggle('active');"></button>
-        <h1>Public Goods</h1>
+        <button className="burger" onclick="this.classList.toggle('active');"></button>
+        <h1 id="company-name">PUBLIC GOODS</h1>
         <div>
           <img id="loupe" src={loupe} />
         </div>
-        <div>
+        <div id="cart-display">
           Cart 0
         </div>
       </header>
