969a8f17b2fb658bb31a93d653ea2bafe61a4ac8
MatthewClarkOSU <matthewclarkosu@gmail.com>
2021-12-23 14:13:18 -0500

    header w/o style in place

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+// import 'font-awesome/css/font-awesome.min.css';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import loupe from "./images/loupe.png"
+import cat_image1_mobile from "./images/cat_image1_mobile.png"
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button class="burger" onclick="this.classList.toggle('active');"></button>
+        <h1>Public Goods</h1>
+        <div>
+          <img id="loupe" src={loupe} />
+        </div>
+        <div>
+          Cart 0
+        </div>
       </header>
+      <body>
+        <img src={cat_image1_mobile}></img>
+      </body>
     </div>
   );
 }
