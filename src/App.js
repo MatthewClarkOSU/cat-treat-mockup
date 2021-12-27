23392f8a153d58e07a90769716ceb476c568ca59
MatthewClarkOSU <matthewclarkosu@gmail.com>
2021-12-27 13:12:37 -0500

    mobile layout correct, desktop almost complete

--- src/App.js
+++ src/App.js
@@ -50,31 +50,36 @@ function App() {
         </div>
 
         <div id="minnows">
+          <h5 id="our-treat-desktop">OUR TREAT</h5>
           <div id="minnows-mobile">
-            <h5 id="our-treat">OUR TREAT</h5>
+            <h5 id="our-treat-mobile">OUR TREAT</h5>
             <img src={minnows} alt="dried minnows treat" />
             <h4 id="treat-name">Freeze Dried Minnows</h4>
             <p>Healthy ingredients with nothing to hide.</p>
           </div>
           <div id="minnows-desktop">
-            <h5 id="our-treat">OUR TREAT</h5>
             <img src={minnows} alt="dried minnows treat" />
             <h4 id="treat-name">Freeze Dried Minnows</h4>
             <p>Healthy ingredients with nothing to hide.</p>
           </div>
         </div>
-          <img id="cat2-mobile" src={cat_image2_mobile} alt="cat high five" />
-
+          
         <div id="membership-info">
-          <h4>PET-APPROVED</h4>
-          <h4>It feels good to be a member</h4>
-          <p>After your free trial, become a full-time member with 
+          <div>
+            <img id="cat2-mobile" src={cat_image2_mobile} alt="cat high five" />
+          </div>
+          <div>
+            <h4>PET-APPROVED</h4>
+            <h4>It feels good to be a member</h4>
+            <p>After your free trial, become a full-time member with
             benefits for $59 a year.</p>
             <ul id="checkmark-bullets">
               <li>Cancel anytime during your free trial.</li>
               <li>Free shipping on orders over $45.</li>
               <li>Direct-to-consumer prices on hundreds of premium products.</li>
             </ul>
+          </div>
+          
         </div>
 
         <div id="bottom-button-container">
