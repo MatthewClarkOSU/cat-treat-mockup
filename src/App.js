3a6b170ae4d378c1396241344a0403de628d9222
MatthewClarkOSU <matthewclarkosu@gmail.com>
2021-12-27 13:02:02 -0500

    organizing divs for desktop layout - css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import loupe from "./images/loupe.png"
 import cat_image1_mobile from "./images/cat_image1_mobile.png"
+import cat_image1_desktop from "./images/cat_image1_desktop.png"
 import minnows from "./images/cat_treats_fd_minnows_01 3.png"
 import cat_image2_mobile from "./images/cat_image2_mobile.png"
 import 'bootstrap-icons/font/bootstrap-icons.css';
@@ -21,30 +22,48 @@ function App() {
 
       <body>
         <div id="free-cat">
-          <img src={cat_image1_mobile} alt="cat with treat" />
-          <h2>Treat your pet (and you). It's on us!</h2>
-          <p>Treat your pet to a healthy treat for <b>free</b> to kick of your Public
-          Goods trial membership.</p>
-          <h3>See what it's like to be a Public Goods member.</h3>
-          <ul>
-            <li>Included in your bundle is a free 2-week membership that gives you
-              unlimited access to our entire collection of sustainable essentials.
-            </li>
-            <br></br>
-            <li>
-              Take a load off. We make it easy for you to make better choices. Always
-              included: eco-friendly products you can trust.
-            </li>
-          </ul>
+          <div id="cat-image1-mobile">
+            <img src={cat_image1_mobile} alt="cat with treat" />
+          </div>
+          <div id="cat-image1-desktop">
+            <img src={cat_image1_desktop} alt="cat with treat" />
+          </div>
+          <div id="treat-your-pet">
+            <h2>Treat your pet (and you). It's on us!</h2>
+            <p>Treat your pet to a healthy treat for <b>free</b> to kick of your Public
+            Goods trial membership.</p>
+            <h3 id="public-goods-member">See what it's like to be a Public Goods member.</h3>
+            <ul>
+              <li>Included in your bundle is a free 2-week membership that gives you
+                unlimited access to our entire collection of sustainable essentials.
+              </li>
+              <br></br>
+              <li>
+                Take a load off. We make it easy for you to make better choices. Always
+                included: eco-friendly products you can trust.
+              </li>
+            </ul>
+            <div id="free-offer-desktop">
+              Claim your free offer
+          </div>
+          </div>
         </div>
 
         <div id="minnows">
-          <h5 id="our-treat">OUR TREAT</h5>
-          <img src={minnows} alt="dried minnows treat" />
-          <h4 id="treat-name">Freeze Dried Minnows</h4>
-          <p>Healthy ingredients with nothing to hide.</p>
+          <div id="minnows-mobile">
+            <h5 id="our-treat">OUR TREAT</h5>
+            <img src={minnows} alt="dried minnows treat" />
+            <h4 id="treat-name">Freeze Dried Minnows</h4>
+            <p>Healthy ingredients with nothing to hide.</p>
+          </div>
+          <div id="minnows-desktop">
+            <h5 id="our-treat">OUR TREAT</h5>
+            <img src={minnows} alt="dried minnows treat" />
+            <h4 id="treat-name">Freeze Dried Minnows</h4>
+            <p>Healthy ingredients with nothing to hide.</p>
+          </div>
         </div>
-        <img id="cat2-mobile" src={cat_image2_mobile} alt="cat high five" />
+          <img id="cat2-mobile" src={cat_image2_mobile} alt="cat high five" />
 
         <div id="membership-info">
           <h4>PET-APPROVED</h4>
