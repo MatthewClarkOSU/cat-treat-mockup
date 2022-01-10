3be1f9405878b263e58400f3c0cf5d7a68986958
MatthewClarkOSU <matthewclarkosu@gmail.com>
2022-01-10 14:12:04 -0500

    changed ids into classes

--- src/App.js
+++ src/App.js
@@ -12,28 +12,28 @@ function App() {
     <div className="App">
       <header className="App-header">
         <button className="burger" onclick="this.classList.toggle('active');"></button>
-        <h1 id="company-name">PUBLIC GOODS</h1>
+        <h1 className="company-name">PUBLIC GOODS</h1>
         <div>
-          <img id="loupe" src={loupe} alt="magnifying glass" />
+          <img className="loupe" src={loupe} alt="magnifying glass" />
         </div>
-        <div id="cart-display">
+        <div className="cart-display">
           Cart 0
         </div>
       </header>
 
       <body>
-        <div id="free-cat">
-          <div id="cat-image1-mobile">
+        <div className="free-cat">
+          <div className="cat-image1-mobile">
             <img src={cat_image1_mobile} alt="cat with treat" />
           </div>
           <div>
-            <img id="cat-image1-desktop" src={cat_image1_desktop} alt="cat with treat" />
+            <img className="cat-image1-desktop" src={cat_image1_desktop} alt="cat with treat" />
           </div>
-          <div id="treat-your-pet">
+          <div className="treat-your-pet">
             <h2>Treat your pet (and you). <br></br> It's on us!</h2>
             <p>Treat your pet to a healthy treat for <b>free</b> to kick of your Public
             Goods trial membership.</p>
-            <h3 id="public-goods-member">See what it's like to be a Public Goods member.</h3>
+            <h3 className="public-goods-member">See what it's like to be a Public Goods member.</h3>
             <ul>
               <li>Included in your bundle is a free 2-week membership that gives you
                 unlimited access to our entire collection of sustainable essentials.
@@ -44,40 +44,40 @@ function App() {
                 included: eco-friendly products you can trust.
               </li>
             </ul>
-            <div id="free-offer-desktop">
+            <div className="free-offer-desktop">
               Claim your free offer
           </div>
           </div>
         </div>
 
-        <div id="minnows">
-          <h5 id="our-treat-desktop">OUR TREAT</h5>
-          <div id="minnows-mobile">
-            <h5 id="our-treat-mobile">OUR TREAT</h5>
+        <div className="minnows">
+          <h5 className="our-treat-desktop">OUR TREAT</h5>
+          <div className="minnows-mobile">
+            <h5 className="our-treat-mobile">OUR TREAT</h5>
             <img src={minnows} alt="dried minnows treat" />
-            <h4 id="treat-name">Freeze Dried Minnows</h4>
-            <p>Healthy ingredients with nothing to hide.</p>
+            <h4 className="treat-name">Freeze Dried Minnows</h4>
+            <p>Healthy ingredients with nothing to hclassNamee.</p>
           </div>
-          <div id="minnows-desktop">
+          <div className="minnows-desktop">
             <img src={minnows} alt="dried minnows treat" />
-            <h4 id="treat-name">Freeze Dried Minnows</h4>
-            <p>Healthy ingredients with nothing to hide.</p>
+            <h4 className="treat-name">Freeze Dried Minnows</h4>
+            <p>Healthy ingredients with nothing to hclassNamee.</p>
           </div>
         </div>
           
-        <div id="membership-info">
-          <div id="cat-image2-mobile">
+        <div className="membership-info">
+          <div className="cat-image2-mobile">
             <img  src={cat_image2_mobile} alt="cat high five" />
           </div>
           <div>
-            <img id="cat-image2-desktop" src={cat_image2_desktop} alt="cat high five" />
+            <img className="cat-image2-desktop" src={cat_image2_desktop} alt="cat high five" />
           </div>
-          <div id="pet-approved">
+          <div className="pet-approved">
             <h4>PET-APPROVED</h4>
             <h4>It feels good to be a member</h4>
             <p>After your free trial, become a full-time member with
             benefits for $59 a year.</p>
-            <ul id="checkmark-bullets">
+            <ul className="checkmark-bullets">
               <li>Cancel anytime during your free trial.</li>
               <li>Free shipping on orders over $45.</li>
               <li>Direct-to-consumer prices on hundreds of premium products.</li>
@@ -85,17 +85,17 @@ function App() {
           </div>
         </div>
 
-        <div id="bottom-button-container">
-          <div id="free-offer">
+        <div className="bottom-button-container">
+          <div className="free-offer">
             Claim your free offer
           </div>
-          <div id="bottom-buttons">
+          <div className="bottom-buttons">
             <i class="bi bi-chevron-left"></i>
             <i class="bi bi-chevron-right"></i>
             <i class="bi bi-box-arrow-up"></i>
             <i class="bi bi-book"></i>
             <i class="bi bi-app"></i>
-            <hr id="bottom-bar" />
+            <hr className="bottom-bar" />
           </div>
 
         </div>
